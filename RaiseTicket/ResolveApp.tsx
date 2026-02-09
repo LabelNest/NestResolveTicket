a198300a6bfee36e509e2456b0a50d1647ec1849
PhalaUdupa <phalachandra.udupa@labelnest.in>
2026-02-09 20:01:30 +0530

    Update ResolveApp.tsx
    importing supabaseclient edited

--- RaiseTicket/ResolveApp.tsx
+++ RaiseTicket/ResolveApp.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect } from 'react';
-import { supabase } from './supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 import {
   Ticket,
   TicketStatus,
